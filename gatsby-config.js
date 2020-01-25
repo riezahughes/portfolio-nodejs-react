3c1b86f8243d84c424adb77c0d3916440d5cf3d5
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2020-01-25 22:01:30 +0000

    Updated package.json and started setting up gatsby correctly for using wp to create blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Daniel Ruxton - Full Stack Developer',
+    description: 'A professional portfolio, blog and CV of Daniel Ruxton',
+    siteUrl: 'https://danielruxton.dev', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://danielruxton.dev',
       },
     },
     'gatsby-plugin-emotion',
